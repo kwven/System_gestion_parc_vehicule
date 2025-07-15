7233d306ea3884e5d280383dd0559ee336a7a3d2
ahmed <lamrania765@gmail.com>
2025-07-15 18:45:02 +0100

    update admin

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Agent, User
 
-# Register your models here.
+# Enregistrement du modèle Agent
+admin.site.register(Agent)
+
+# Enregistrement du modèle User personnalisé
+# Utilise UserAdmin pour conserver les fonctionnalités d'administration par défaut de Django pour les utilisateurs
+admin.site.register(User, UserAdmin)
