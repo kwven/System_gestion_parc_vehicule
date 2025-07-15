29b6888a99d3746ab885b0fec2462885527e9f4a
ahmed <lamrania765@gmail.com>
2025-07-15 18:52:33 +0100

    update views for new serializers

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .models import Agent, User
+from .serializers import AgentSerializer, UserSerializer
 
-# Create your views here.
+class AgentViewSet(viewsets.ModelViewSet):
+    queryset = Agent.objects.all()
+    serializer_class = AgentSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+
+class CustomTokenRefreshView(TokenRefreshView):
+    permission_classes = (AllowAny,)
+
+# Vue pour l'enregistrement d'un nouvel utilisateur (si nécessaire)
+class RegisterUserView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(request.data["password"])
+            user.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
