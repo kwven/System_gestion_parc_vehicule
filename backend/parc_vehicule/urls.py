24b7833bf4cb12b9e60c6ed56259c3e2925694c9
ahmed <lamrania765@gmail.com>
2025-07-18 06:10:14 +0100

    urls

--- backend/parc_vehicule/urls.py
+++ backend/parc_vehicule/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for vehicle_fleet project.
+URL configuration for parc_vehicule project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -16,10 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
