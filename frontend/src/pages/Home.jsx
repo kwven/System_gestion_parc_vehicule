0d736943624450d2ae1bb035b38ec722efa27259
ahmed <lamrania765@gmail.com>
2025-07-22 09:29:07 +0100

    first thing

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../components/common/Navbar';
+function Home() {
+  return (
+    <div className='bg-[#020202]'>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Home;
