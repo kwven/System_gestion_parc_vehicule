b27cc43833794991c82b6e6282f67c1ae1028919
ahmed <lamrania765@gmail.com>
2025-07-23 00:18:02 +0100

    updates

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,5 +1,6 @@
 import React,{useState} from 'react';
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 function Navbar() {
     const  [nav,setNav] = useState(false);
     const handleNav = () => {
@@ -7,25 +8,13 @@ function Navbar() {
     }
     return (
         <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white">
-            <h1 className="w-full text-3xl font-bold text-[#00df9a]">SGVP</h1>
+            <h1 className="w-full text-3xl font-bold text-[#d6d8db]">SGVP</h1>
             <ul className='hidden md:flex'>
-                <li className="p-4">Home</li>
-                <li className="p-4">About</li>
-                <li className="p-4">Help</li>
-                <li className="p-4">Login</li>
+                <li> <Link to="/" className="p-4">Home</Link> </li>
+                <li> <Link to="/About" className="p-4">About</Link> </li>
+                <li> <Link to="/Help" className="p-4">Help</Link> </li>
+                <li> <Link to="/Login" className="p-4">Login</Link> </li>
             </ul>
-            <div onClick={handleNav} className='block md:hidden'>
-                {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
-            </div>
-            <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'} >
-                <h1 className='w-full text-3xl font-bold text-[#00df9a]'>SGVP</h1>
-                <ul className="uppercase p-4">
-                    <li className="p-4 border-b border-gray-600 ">Home</li>
-                    <li className="p-4 border-b border-gray-600">About</li>
-                    <li className="p-4 border-b border-gray-600">Help</li>
-                    <li className="p-4 border-b border-gray-600">Login</li>
-                </ul>
-            </div>
         </div>
     );
     }
